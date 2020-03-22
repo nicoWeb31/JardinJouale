bab79cc86f55b7cac19c451fa9b33231335ed6fc
Riot Nicolas <nico.riot@free.fr>
2020-03-22 19:27:56 +0100

    crud legume

--- src/Controller/Admin/AdminLegumeController.php
+++ src/Controller/Admin/AdminLegumeController.php
@@ -2,16 +2,54 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Legume;
+use App\Form\LegumeType;
+use App\Repository\LegumeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminLegumeController extends AbstractController
 {
     /**
      * @Route("/admin/legumes", name="admin_legumes")
      */
-    public function index()
+    public function AdminLegumes(LegumeRepository $repo)
     {
-        return $this->render('admin_legume/liste.html.twig');
+        $legumes = $repo->findAll();
+        return $this->render('admin_legume/adminListe.html.twig',[
+            'legumes'=>$legumes
+        ]);
+    }
+
+    /**
+     * @Route("/admin/legumes/creation", name="admin.legumes.creation")
+     * @Route("/admin/legumes/{id}", name="admin.legumes.modif")
+     */
+    public function AdminLegumeCreate(Legume $legume = null, EntityManagerInterface $man, Request $req)
+    {
+
+        if(!$legume)
+        {
+            $legume = new Legume();
+        }
+
+        $form = $this->createForm(LegumeType::class,$legume);
+        $form->handleRequest($req);
+
+
+        if($form->isSubmitted() && $form->isValid()){
+            $man->persist($legume);
+            $man->flush();
+            return $this->redirectToRoute('admin_legumes');
+
+        }
+
+        return $this->render('admin_legume/adminModiLegume.html.twig',[
+            'legume'=>$legume,
+            "form"=>$form->createView(),
+            "isModif" => $legume->getId() !== null
+        ]);
     }
 }
